97763ccd5229aff9a35397801d01c22ba8f93f91
Gregory Soszynski <gegory.soszynski.dev@gmail.com>
2020-06-16 14:25:05 -0400

    firebase

--- store/actions/products.js
+++ store/actions/products.js
@@ -2,20 +2,38 @@ export const DELETE_PRODUCT = 'DELETE_PRODUCT';
 export const CREATE_PRODUCT = 'CREATE_PRODUCT';
 export const UPDATE_PRODUCT = 'UPDATE_PRODUCT';
 
-
 export const deleteProduct = productId => {
   return { type: DELETE_PRODUCT, pid: productId };
 };
 
 export const createProduct = (title, description, imageUrl, price) => {
-  return {
-    type: CREATE_PRODUCT,
-    productData: {
-      title,
-      description,
-      imageUrl,
-      price
-    }
+  return async dispatch => {
+    // any async code you want!
+    const response = await fetch('https://shopp-app-7085f.firebaseio.com//products.json', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        title,
+        description,
+        imageUrl,
+        price
+      })
+    });
+
+    const resData = await response.json();
+
+    dispatch({
+      type: CREATE_PRODUCT,
+      productData: {
+        id: resData.name,
+        title,
+        description,
+        imageUrl,
+        price
+      }
+    });
   };
 };
 
